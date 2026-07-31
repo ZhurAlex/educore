559905998f9e6ac8adb66ad8f309ebb999adb14e
ZhurAlex <265831799+ZhurAlex@users.noreply.github.com>
2026-07-31 12:34:20 +0300

    Specs fix

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -8,21 +8,4 @@ RSpec.describe Option, type: :model do
   it "requires a body" do
     expect(build(:option, body: nil)).not_to be_valid
   end
-
-  it "does not allow two correct options on the same question" do
-    question = create(:question, answer_type: :multiple_choice, correct_answer: nil)
-    create(:option, question: question, correct: true)
-
-    second = build(:option, question: question, correct: true)
-    expect(second).not_to be_valid
-    expect(second.errors[:correct]).to be_present
-  end
-
-  it "allows multiple incorrect options on the same question" do
-    question = create(:question, answer_type: :multiple_choice, correct_answer: nil)
-    create(:option, question: question, correct: false)
-
-    second = build(:option, question: question, correct: false)
-    expect(second).to be_valid
-  end
 end
